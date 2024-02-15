7de65fdbd5d2174c12b1c24f3f7c7bccc01c790d
iabdullahbayram <abdullahbayram1997@hotmail.com>
2024-02-15 18:56:18 +0300

    Proje dosyası ekle.

---
+++ Fizzbuzz2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger;
+            Console.WriteLine("bir deger gir");
+            string c = Console.ReadLine();
+            deger = Convert.ToInt32((string)c);
+
+            for (int i = 1; i <= deger; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
